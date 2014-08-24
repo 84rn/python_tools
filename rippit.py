412bee8359773a50e82d606426d98573a9dc1584
Bartek Bartyzel <barbart1@gmail.com>
2014-08-24 21:34:53 +0200

    Updated rippit.py

--- rippit.py
+++ rippit.py
@@ -4,6 +4,13 @@ import argparse
 import urllib
 import re
 
+# TODO:
+#   - divide requests into threads
+#   - define FINISH argument when -pe is not provided
+#   - check if not overwriting, ask user if should (Y/N/A)
+#   - add parameter that forces overwriting
+#   - check for errors: page load, img retrieve
+
 parser = argparse.ArgumentParser(description='Batch-download pictures from a subreddit.')
 parser.add_argument("subreddit", help="name of the subreddit from which you want to rip")
 parser.add_argument("-ps", help="start page (default: 0)", metavar='#', default=0, type=int)
@@ -13,6 +20,7 @@ parser.add_argument("-t",  help="time window (default: day)", \
                     choices=['day', 'week', 'month', 'year', 'all'], default='day')
 parser.add_argument("-s",  help="sorting method (default: hot)", \
                     choices=['new', 'hot', 'top'], default='hot')
+parser.add_argument("-f", "--force",  help="force overwriting", action="store_true")
 parser.add_argument("-v", "--verbose", help="show additional information", action="store_true")
 args = parser.parse_args()
 
@@ -50,17 +58,19 @@ for p in range(p_start, p_end):
         img_element = imglist[0][i][0][0]
         desc_element = imglist[0][i][1][0]
         type_element = imglist[0][i][1][1]
-        type_text = type_element.text.strip()[:5]
-      
+        type_text = type_element.text.strip()[:5]      
         img_url = img_element.get("src")
         desc = replaceIllegalChars(desc_element.text).encode('ascii', 'ignore')                       
         img_url = 'http:' + img_url[:-5] + img_url[-4:]
         type = 'gif'
         if(type_text == 'image'):
             type = 'jpg'
-        save_path = save_dir + '\\' + desc + '.' + type
+        filename = desc + '.' + type
+        save_path = save_dir + '\\' + filename
         if args.verbose:
-            print 'Saving [' + str(num_pics) + '] ' + desc + '.' + type
+            print 'Saving [' + str(num_pics) + '] ' + filename
+        if(os.path.exists(save_path)):
+            print 'File \'' + filename + '\' already exists. Overwrite? [Y/N/A]'
         urllib.urlretrieve(img_url, save_path)
         num_pics = num_pics + 1
 print 'Finished saving ' + str(num_pics - 1) + ' pictures from ' + str(p_end - p_start) + ' page(s).'
